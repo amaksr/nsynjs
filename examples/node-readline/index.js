b505442d07be37b7f152426fb9609c951a6d6eae
amaksr <amaksr@gmail.com>
2017-06-03 15:49:07 -0600

    added delay

--- examples/node-readline/index.js
+++ examples/node-readline/index.js
@@ -3,9 +3,10 @@
  */
 
 var textFile = require('../../wrappers/nodeReadline').textFile;
+var wait = require('../../wrappers/nsynWait').nsynWait;
 var nsynjs = require('../../nsynjs');
 
-function process(textFile) {
+function process(textFile,wait) {
     var fh = new textFile();
     fh.open('../data/lorem.txt');
     var s, i = 0;
@@ -15,10 +16,11 @@ function process(textFile) {
             console.log(s);
         else
             console.log("<empty line>");
+        wait(synjsCtx,1000);
     }
     fh.close();
 }
 
-var ctx = nsynjs.run(process,{},textFile,function () {
+var ctx = nsynjs.run(process,{},textFile,wait,function () {
     console.log('done');
 });
